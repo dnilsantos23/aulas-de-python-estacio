a8898c595e1cfd7914e4293b3bc3df828f177e5e
dnilsantos23 <danielinfo23@gmail.com>
2025-04-04 00:26:42 -0300

    update

--- trabalhando-com-textos.py
+++ trabalhando-com-textos.py
@@ -1,3 +1,3 @@
 arquivoTeste = open('teste.txt', 'w')
 arquivoTeste.write('Teste de escrita em arquivo texto\n')
-open('teste.txt', 'a')
+open('teste.txt', 'a').write('Teste de escrita em arquivo texto\n')
