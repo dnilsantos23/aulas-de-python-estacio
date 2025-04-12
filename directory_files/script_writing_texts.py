fb0b519d146a01183a514d33ea6abd86c1636eb9
dnilsantos23 <danielinfo23@gmail.com>
2025-04-12 19:57:03 -0300

    Add project configuration files and text transfer script
    - Created .gitignore to exclude unnecessary files
    - Added project module configuration in Estacio.iml
    - Defined inspection profiles in profiles_settings.xml
    - Configured project settings in misc.xml
    - Established module management in modules.xml
    - Set up version control in vcs.xml
    - Implemented text transfer functionality in script_writing_texts.py
    - Added source and destination text files for processing

---
+++ directory_files/script_writing_texts.py
@@ -0,0 +1,36 @@
+import os
+
+def tranfer_texts(arquivo_origem, arquivo_destino):
+    try:
+        with open(arquivo_origem, 'r', encoding='utf-8') as file_origem:
+            conteudo = file_origem.read();
+    except FileNotFoundError:
+        print(f'Arquivo {arquivo_origem} não encontrado!')
+        return
+    except PermissionError:
+        print(f'Sem permissão para acessar o arquivo {arquivo_origem}')
+        return
+    except Exception as e:
+        print(f'Erro inesperado ao ler o arquivo {arquivo_origem}', e)
+        return
+    try:
+        with open(arquivo_destino, 'w' , encoding='utf-8') as file_destino:
+            file_destino.write('Cabeçalho: Conteúdo do Arquivo de Destino\n')
+            file_destino.write(conteudo)
+            print(f'Conteúdo escrito com Sucesso em {arquivo_destino}')
+    except PermissionError:
+        print(f'Sem permissões para acessar o arquivo {arquivo_destino}.')
+        return
+    except Exception as e:
+        print(f'Erro desconhecido ao acessar o arquivo {arquivo_destino}', e)
+        return
+
+def main():
+    directory_files = "directory_files"
+    arquivo_origem = os.path.join(directory_files, 'arquivo_origem1.txt')
+    arquivo_destino = os.path.join(directory_files, 'arquivo_destino.txt')
+    tranfer_texts(arquivo_origem, arquivo_destino)
+
+
+if __name__ == "__main__":
+    main()
