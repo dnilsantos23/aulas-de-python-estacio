7f1a340d6b67510f5b18a0b25c4f8d4cfcd14091
dnilsantos23 <danielinfo23@gmail.com>
2025-04-28 01:29:06 -0300

    Refactor database connection and table creation in first_db.py

--- DATABASE/first_db.py
+++ DATABASE/first_db.py
@@ -1,20 +1,36 @@
-#
+# Este código é um exemplo de como criar um banco de dados SQLite, criar uma tabela e inserir dados nela.
 import sqlite3 as conector
 
- # Abertura de conexão
-conexao = conector.connect("URL SQLite")
+try:
+    # Abertura de conexão
+    conexao = conector.connect("./banco_de_dados.db")
 
- # Aquisição de um cursor
-cursor = conexao.cursor()
+    # Aquisição de um cursor
+    cursor = conexao.cursor()
 
- # Execução comandos: SELECT..CREATE...INSERT..UPDATE..DELETE
-cursor.execute("CREATE TABLE IF NOT EXISTS clientes (id INTEGER PRIMARY KEY, nome TEXT, idade INTEGER)")
-cursor.execute("INSERT INTO clientes (nome, idade) VALUES ('João', 30)")
-cursor.fetchall()
+    # Execução comandos: SELECT..CREATE...INSERT..UPDATE..DELETE
+    comando =  '''CREATE TABLE IF NOT EXISTS Pessoa (
+                    cpf INTEGER NOT NULL,
+                    nome TEXT NOT NULL,
+                    nascimento DATE NOT NULL,
+                    oculos BOOLEAN NOT NULL,
+                    PRIMARY KEY (cpf));'''
+    # Efetivação do comando
+    cursor.execute(comando)
 
- # Efetivação do comando
-conexao.commit()
-  
- # Fechamento das conexões
-cursor.close()
-conexao.close()
+    # Inserção de dados na tabela
+    comando = '''INSERT INTO Pessoa (cpf, nome, nascimento, oculos) VALUES
+                (12345678900, 'João', '1990-01-01', 1),
+                (98765432100, 'Maria', '1985-05-15', 0),
+                (45678912300, 'José', '1992-10-20', 1);'''
+    # Efetivação do comando
+    cursor.execute(comando)
+    conexao.commit()
+    print("Dados inseridos com sucesso!")
+except conector.DatabaseError as erro:
+    print("Erro de banco de dados: ", erro)      
+finally:
+    if conexao:
+        cursor.close()
+        conexao.close()
+
